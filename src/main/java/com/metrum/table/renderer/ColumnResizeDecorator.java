dcec13dcb1a24b5d2fff178212f6528402011c2d
Leandro Lima <leandro.lima@memt.com.br>
2016-08-05 13:14:55 -0300

    O ColumnResizeDecorator foi reformulado e passa a funcionar.

--- src/main/java/com/metrum/table/renderer/ColumnResizeDecorator.java
+++ src/main/java/com/metrum/table/renderer/ColumnResizeDecorator.java
@@ -7,7 +7,6 @@ package com.metrum.table.renderer;
 
 import java.awt.Component;
 import javax.swing.JTable;
-import javax.swing.table.JTableHeader;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 
@@ -17,47 +16,103 @@ public class ColumnResizeDecorator extends TableCellRendererDecorator {
 
         MIN, MAX, NONE
     };
+    
     private final ColumnResizeMode mode;
     private final int columnPadding;
+    private final boolean considerData;
+    private final boolean considerHeader;
+    
+    public ColumnResizeDecorator(TableCellRenderer delagated, ColumnResizeMode mode, 
+            boolean considerHeader, boolean considerData, int columnPadding) {
+        super(delagated);
+        
+        this.mode = mode;
+        this.considerHeader = considerHeader;
+        this.considerData = considerData;
+        this.columnPadding = columnPadding;
+    }
 
     public ColumnResizeDecorator(TableCellRenderer delagated, ColumnResizeMode mode,
             int columnPadding) {
         super(delagated);
         this.mode = mode;
         this.columnPadding = columnPadding;
+        this.considerHeader = true;
+        this.considerData = true;
     }
 
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value,
             boolean isSelected, boolean hasFocus, int row, int col) {
-        Component component = super.getTableCellRendererComponent(table,
-                value, isSelected, hasFocus, row, col);
-
-        final JTableHeader header = table.getTableHeader();
 
         final TableColumn column = table.getColumnModel().getColumn(col);
-        final String headerTitle = (String) column.getHeaderValue();
-        final int headerWidth = Math.max(header.getFont().getSize() * headerTitle.length(),
-                columnPadding) + columnPadding;
-        column.setMaxWidth(Integer.MAX_VALUE);
-        column.setMinWidth(headerWidth);
-        int preferredWidth = headerWidth;
-        preferredWidth = Math.max(component.getPreferredSize().width, Math.max(preferredWidth,
-                column.getWidth()));
-
+        int width = 0;
+        if(considerHeader)
+            width = getColumnHeaderWidth(table, col);
+        
+        if(considerData)
+            width = Math.max(getColumnDataWidth(table, col), width);
+        
+        width += 2 * columnPadding;
+        
         switch (mode) {
             case MAX:
-                column.setMinWidth(preferredWidth);
+                column.setMinWidth(width);
                 break;
             case MIN:
-                column.setMaxWidth(preferredWidth);
+                column.setMaxWidth(width);
                 break;
             case NONE:
-                column.setPreferredWidth(preferredWidth);
+                column.setPreferredWidth(width);
+                break;
+        }
+        
+        return super.getTableCellRendererComponent(table, value, isSelected,
+                hasFocus, row, col);
+    }
+    
+    /**
+     * Calculate the width based on the widest cell renderer for the given column.
+     */
+    private int getColumnDataWidth(JTable table, int column) {
+        int preferredWidth = 0;
+        int maxWidth = table.getColumnModel().getColumn(column).getMaxWidth();
+
+        for (int row = 0; row < table.getRowCount(); row++) {
+            preferredWidth = Math.max(preferredWidth, getCellDataWidth(table, row, column));
+
+            //  We've exceeded the maximum width, no need to check other rows
+            if (preferredWidth >= maxWidth)
                 break;
         }
 
-        return component;
+        return preferredWidth;
+    }
+
+    /**
+     * Calculated the width based on the column name
+     */
+    private int getColumnHeaderWidth(JTable table, int column) {
+        TableColumn tableColumn = table.getColumnModel().getColumn(column);
+        Object value = tableColumn.getHeaderValue();
+        TableCellRenderer renderer = tableColumn.getHeaderRenderer();
+
+        if (renderer == null)
+            renderer = table.getTableHeader().getDefaultRenderer();
+
+        Component c = renderer.getTableCellRendererComponent(table, value, false, false, -1, column);
+        return c.getPreferredSize().width;
+    }
+
+    /**
+     * Get the preferred width for the specified cell
+     */
+    private int getCellDataWidth(JTable table, int row, int column) {      
+        final Object value = table.getValueAt(row, column);
+        final Component component = delegated.getTableCellRendererComponent(table, value, false, 
+                false, row, column);
+        final int width = component.getPreferredSize().width + table.getIntercellSpacing().width;
+        return width;    
     }
 
 }
